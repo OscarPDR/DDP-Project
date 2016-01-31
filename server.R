171c72ce3bf072c9e8bf80797b952505a2aef5c0
Oscar Peña <oscar.pdr@gmail.com>
2016-01-31 12:30:48 +0100

    Updated analysis with two views: one for a quick overview of the variables, an another with with a basic facet filtering table

--- server.R
+++ server.R
@@ -4,29 +4,24 @@ library(ggplot2)
 # Define the server logic required
 shinyServer(function(input, output) {
     
-    output$carat <- renderUI({
-        sliderInput(
-            "carat", 
-            "Carat",
-            min(diamonds$carat),
-            max(diamonds$carat),
-            c(min(diamonds$carat), max(diamonds$carat))
+    output$main_table <- DT::renderDataTable(DT::datatable({
+            data <- diamonds
+            
+            if (input$cut != 'All') {
+                data <- data[data$cut == input$cut,]
+            }
+            if (input$color != 'All') {
+                data <- data[data$color == input$color,]
+            }
+            if (input$clarity != 'All') {
+                data <- data[data$clarity == input$clarity,]
+            }
+            
+            data
+        },
+        options=list(
+            searching=FALSE,
+            pageLength=15
         )
-    })
-    
-    output$cut <- renderUI({
-        checkboxGroupInput(
-            "cut", 
-            "Cut",
-            choices=setNames(as.list(levels(diamonds$cut)), levels(diamonds$cut))
-        )
-    })
-    
-    output$color <- renderUI({
-        checkboxGroupInput(
-            "color", 
-            "Color",
-            choices=setNames(as.list(levels(diamonds$color)), levels(diamonds$color))
-        )
-    })
+    ))
 })
--- ui.R
+++ ui.R
@@ -1,41 +1,144 @@
 library(shiny)
+library(ggplot2)
 
 # Define UI for the web application
-shinyUI(fluidPage(
-  
-    # Application title
-    titlePanel("Diamond prices"),
-  
-    sidebarLayout(
-        # sidebar
-        sidebarPanel(
-            p(
-                "This dataset (included in the 'ggplot2' package, contains price information for 
-                nearly 54K diamonds, based on 10 different variables.",
-                br(),
-                br(),
-                align="center",
-                img(src="diamond.png", height="114", width="200"),
-                br()
-            ),
-            p("The variables are as follows:"),
-            uiOutput("carat"),
-            
+shinyUI(
+    navbarPage(
+        'Diamond prices',
+        tabPanel(
+            'Overview',
             fluidRow(
                 column(
-                    6,
-                    uiOutput("cut")
+                    4,
+                    br(),
+                    br(),
+                    br(),
+                    p(
+                        img(src='diamond.png', height='114', width='200'),
+                        align='center'
+                    )
                 ),
                 column(
                     6,
-                    uiOutput("color")
+                    p(
+                        "This dataset (included in the 'ggplot2' package, contains price information for 
+                        nearly 54K diamonds, based on 10 different variables."
+                    ),
+                    br(),
+                    p(
+                        "The variables are described as:"
+                    ),
+                    withMathJax(),
+                    tags$ul(
+                        tags$li(
+                            strong("Price: "),
+                            "Price in US dollars"
+                        ),
+                        tags$li(
+                            strong("Carat: "),
+                            "Weight of the diamond"
+                        ),
+                        tags$li(
+                            strong("Cut: "),
+                            "Quality of the cut"
+                        ),
+                        tags$li(
+                            strong("Color: "),
+                            "Diamond color"
+                        ),
+                        tags$li(
+                            strong("Clarity: "),
+                            "A measurement of how clear the diamond is"
+                        ),
+                        tags$li(
+                            strong("X: "),
+                            "Length in mm"
+                        ),
+                        tags$li(
+                            strong("Y: "),
+                            "Width in mm"
+                        ),
+                        tags$li(
+                            strong("Z: "),
+                            "Depth in mm"
+                        ),
+                        tags$li(
+                            strong("Depth: "),
+                            "Total depth percentage: $$depth_\\%=\\frac{z}{mean(x, y)}$$"
+                        ),
+                        tags$li(
+                            strong("Table: "),
+                            "Width of top of diamond relative to widest point"
+                        )
+                    ),
+                    br(),
+                    br(),
+                    p(
+                        "You can find more information about how diamonds are measured in ",
+                        a(
+                            "this link.",
+                            href="http://shop.diamondideals.com/education/find/four_cs.cfm"
+                        )
+                    )
                 )
             )
         ),
-    
-        # main
-        mainPanel(
-            plotOutput("distPlot")
+        tabPanel(
+            'Data table',
+            fluidRow(
+                column(2),
+                column(
+                    6,
+                    p(
+                        "In this panel, you can easily filter the data based on three factor variables,
+                        in order to consult those dataset rows which match the selected values"
+                    )
+                )
+            ),
+            br(),
+            fluidRow(
+                column(2),
+                column(
+                    3,
+                    selectInput(
+                        'cut',
+                        'Cut',
+                        c(
+                            "All",
+                            unique(as.character(diamonds$cut))
+                        )
+                    )
+                ),
+                column(
+                    3,
+                    selectInput(
+                        'color',
+                        'Color',
+                        c(
+                            "All",
+                            unique(as.character(diamonds$color))
+                        )
+                    )
+                ),
+                column(
+                    3,
+                    selectInput(
+                        'clarity',
+                        'Clarity',
+                        c(
+                            "All",
+                            unique(as.character(diamonds$clarity))
+                        )
+                    )
+                )
+            ),
+            fluidRow(
+                column(1),
+                column(
+                    10,
+                    DT::dataTableOutput('main_table')
+                )
+            )
         )
     )
-))
+)
